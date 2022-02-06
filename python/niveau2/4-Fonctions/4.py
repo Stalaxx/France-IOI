936e357e52722243b30f09aa66554bc4931346fe
Prog'z <39605375+ThomasProg@users.noreply.github.com>
2022-02-06 22:55:44 +0100

    python > niveau2 > 4-Fonctions : added

---
+++ python/niveau2/4-Fonctions/4.py
@@ -0,0 +1,13 @@
+def AfficheLigne(nbColonnes, motif):
+    for i in range(nbColonnes):
+        print(motif, end="")
+    print()
+
+
+nbLignes = int(input())
+nbColonnes = int(input())
+motif = input()
+
+for i in range(nbLignes):
+    AfficheLigne(nbColonnes, motif)
+
